1a256a2cc818e5cf87610db1f5f3fbe864a8fac2
Toma1402 <toma.k1402@gmail.com>
2023-02-25 14:35:07 +0200

    s

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,12 @@
 import PropTypes from 'prop-types';
 import { ContactItem } from 'components/ContactItem/ContactItem';
 import { useSelector } from 'react-redux';
-import { selectContacts } from 'redux/selectors';
+import { selectFilteredContacts } from 'redux/selectors';
 export const ContactList = () => {
-  const inputedFilter = useSelector(state => state.filter);
-  const contactsArr = useSelector(selectContacts);
-  const normalizedFilter = inputedFilter.toLowerCase();
-  const newArray = contactsArr.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-  console.log(contactsArr);
+  const filteredContacts = useSelector(selectFilteredContacts);
   return (
     <ul>
-      {newArray.map(({ name, id, phone }, index) => (
+      {filteredContacts.map(({ name, id, phone }, index) => (
         <ContactItem key={id} name={name} idx={id} phone={phone} />
       ))}
     </ul>
